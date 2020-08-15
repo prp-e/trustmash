a8495fe08b3c0530fac555d6e9ded71d0065bf07
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-08-15 19:16:56 +0430

    It works really cool now

--- app.rb
+++ app.rb
@@ -2,12 +2,27 @@ require 'sqlite3'
 require 'sinatra'
 require 'json'
 
-input = File.open("clean_text.txt", 'r')
+input_strings = File.open("clean_text.txt", 'r')
 phrases = []
 
+input_strings.readlines.each do |line|
+    phrases << line 
+end
+
 db = SQLite3::Database.open('db.sqlite3')
+db.execute("DROP TABLE IF EXISTS trusts")
 db.execute("CREATE TABLE IF NOT EXISTS trusts(id INTEGER PRIMARY KEY AUTOINCREMENT, trusty_case TEXT, rating INTEGER)")
+
+phrases.each do |string|
+    db.execute("INSERT INTO trusts (trusty_case, rating) values (\"#{string}\", 0)")
+end
 
 get '/' do 
     'Nothing special'
 end 
+
+get '/:id' do 
+    content_type :json 
+    text = db.execute("SELECT trusty_case FROM trusts WHERE id=#{params[:id]}")
+    {:id => params[:id], :text => text}.to_json
+end
