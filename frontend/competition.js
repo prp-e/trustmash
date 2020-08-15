6e2971648e1228f14462d8eacd4550a499ee90ab
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-08-16 02:37:59 +0430

    competition links generated

--- frontend/competition.js
+++ frontend/competition.js
@@ -11,5 +11,8 @@ competitor_2 = Math.floor(1 + (Math.random() * 514 ));
 link_competitor_1 = "http://localhost:4567/" + String(competitor_1);
 link_competitor_2 = "http://localhost:4567/" + String(competitor_2);
 
+competition_link_1 = "http://localhost:4567/rate/" + String(competitor_1) + "/" + String(competitor_2)
+competition_link_2 = "http://localhost:4567/rate/" + String(competitor_2) + "/" + String(competitor_1)
+
 request_1 = fetch(link_competitor_1).then(res => {return res.json()}).then(data => {appendData(data['text'], "competitor_1")}).catch(error => {console.log(error)})
 request_2 = fetch(link_competitor_2).then(res => {return res.json()}).then(data => {appendData(data['text'], "competitor_2")}).catch(error => {console.log(error)})
