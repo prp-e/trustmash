480df2661865ed3556a4cea20c8e993b62bf3848
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-08-16 03:08:08 +0430

    Try to fix this

--- frontend/competition.js
+++ frontend/competition.js
@@ -14,11 +14,13 @@ link_competitor_2 = "http://localhost:4567/" + String(competitor_2);
 competition_link_1 = "http://localhost:4567/rate/" + String(competitor_1) + "/" + String(competitor_2)
 competition_link_2 = "http://localhost:4567/rate/" + String(competitor_2) + "/" + String(competitor_1)
 
-var competition_1 = document.getElementById("competition_1");
-competition_1.setAttribute("href", competition_link_1);
-
-var competition_2 = document.getElementById("competition_2");
-competition_2.setAttribute("href", competition_link_2);
-
 request_1 = fetch(link_competitor_1).then(res => {return res.json()}).then(data => {appendData(data['text'], "competitor_1")}).catch(error => {console.log(error)});
 request_2 = fetch(link_competitor_2).then(res => {return res.json()}).then(data => {appendData(data['text'], "competitor_2")}).catch(error => {console.log(error)});
+
+function appendCompetitionLink(competition_id, competition_link){
+    var competition = document.getElementById(competition_id);
+    competition.setAttribute("href", competition_link);
+}
+
+appendCompetitionLink("competition_1", competition_link_1);
+appendCompetitionLink("competition_2", competition_link_2); 
